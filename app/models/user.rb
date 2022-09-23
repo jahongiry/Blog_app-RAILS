273b4df23e0b5c1b5c806bea71514856dd628467
Jahongir Yusupov <91022355+jahongiry@users.noreply.github.com>
2022-09-23 18:36:18 +0500

    Fix linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: :user_id
   has_many :comments, foreign_key: :user_id
   has_many :likes, foreign_key: :user_id
-  
+
   def last_three_posts
     posts.last(3)
   end
