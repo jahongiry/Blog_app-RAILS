2b134fb5473b19da2ecdeb6ad021fc6882bd10a2
Jahongir Yusupov <91022355+jahongiry@users.noreply.github.com>
2022-09-28 18:09:47 +0500

    Add required changes

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'PostsController', type: :request do
   context 'GET index' do
     before(:example) do
-      get '/users/1/posts'
+      get '/users/3/posts'
     end
 
     it 'returns a 200' do
@@ -13,15 +13,27 @@ RSpec.describe 'PostsController', type: :request do
     it 'renders the index template' do
       expect(response).to render_template('index')
     end
+
+    it 'response body includes correct placeholder text' do
+      expect(response.body).to include('Post List')
+    end
   end
 
   describe 'GET show' do
     before(:example) do
-      get '/users/2/posts'
+      get '/users/3/posts/6'
     end
 
     it 'returns a 200' do
       expect(response).to have_http_status(200)
+    end
+
+    it 'renders the index template' do
+      expect(response).to render_template('show')
+    end
+
+    it 'response body includes correct placeholder text' do
+      expect(response.body).to include('Show Post Details')
     end
   end
 end
